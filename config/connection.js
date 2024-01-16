6ffd5dba4a30ea59096206fcd94b9a827d38d108
kwubbenhorst <140316693+kwubbenhorst@users.noreply.github.com>
2024-01-15 22:56:35 -0500

    added comment to connection.js file and modified User.js and seeds.

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,12 @@
+/*
+ * This file configures the connection to the MySQL database using Sequelize. Environment variables give
+ * secure storage of database credentials in the .env file which is not tracked on GitHub. When deployed,
+ * Heroku's add on JawsDB will assign an URL, but in development the app can use the specified mySQL 
+ * configuration. The database connection is established by creating a Sequelize instance which is exported
+ * from here and imported by models files and server.js
+ */
 const Sequelize = require('sequelize');
+// Load environment variables from .env file
 require('dotenv').config();
 
 let sequelize;
--- models/User.js
+++ models/User.js
@@ -10,6 +10,9 @@
  * - first_name: User's first name.
  * - last_name: User's last name.
  * - profile_pic: Path or URL to the user's profile picture.
+ * - user_about: Short self-introduction by the user to display with their profile
+ * - city: the city where the user lives
+ * - city_quadrant: the quadrant of the city (SE, SW, NE, NW) where the user lives. 
  * - credit_balance: Current balance of credits for the user.
  * - badge_count: Number of badges earned by the user.
  * - police_check_uploaded: Boolean indicating whether a police check has been uploaded.
@@ -28,32 +31,73 @@
  }
  
  User.init(
-   {
-     id: {
-       type: DataTypes.INTEGER,
-       allowNull: false,
-       primaryKey: true,
-       autoIncrement: true,
-     },
-     name: {
-       type: DataTypes.STRING,
-       allowNull: false,
-     },
-     email: {
-       type: DataTypes.STRING,
-       allowNull: false,
-       unique: true,
-       validate: {
-         isEmail: true,
-       },
-     },
-     password: {
-       type: DataTypes.STRING,
-       allowNull: false,
-       validate: {
-         len: [8],
-       },
-     },
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    profile_pic: {
+      type: DataTypes.STRING,
+      allowNull: true, // Allow users not to have a profile picture
+      validate: {
+        isUrl: true, // Validate that it's a URL.  Application only allowing urls at this point.
+      },
+    },
+    user_about: {
+      type: DataTypes.TEXT,
+      validate: {
+        len: [100, 1250],
+      },
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city_quadrant: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    credit_balance: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    badge_count: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    police_check_uploaded: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
    },
    {
      hooks: {
